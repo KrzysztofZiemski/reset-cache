d232ca48acb80711d65d0c275e108146e53deda6
Krzysztof <krzysztof.ziemski@sagiton.pl>
2022-03-14 08:21:31 +0100

    ccc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import x from '../package.json'
 
 function App() {
+
+ 
+console.log('wersja', x.version)
   return (
     <div className="App">
       <header className="App-header">
